3cb0d453cf2566f590ce54accc7d866845aaac8e
ashish rana <livelifelively@ashishs-MacBook-Pro.local>
2019-03-05 02:05:33 +0530

    auth html css

---
+++ sample.firebase.config.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: 'XXXXXXXX',
+  authDomain: 'example.firebaseapp.com',
+  databaseURL: 'https://example.firebaseio.com',
+  projectId: 'example',
+  storageBucket: 'example.appspot.com',
+  messagingSenderId: '1111111111'
+}
--- src/views/GuestLanding.vue
+++ src/views/GuestLanding.vue
@@ -7,9 +7,41 @@
       <body-vertical-scroll>
         <div slot="content">
           <el-row :gutter="15">
-            <el-col :span="12">hello</el-col>
-            <el-col :span="12">
-              <login-form-vertical></login-form-vertical>
+            <el-col :span="16">
+              <div class="description">
+                <span style="color: #409EFF; font-weight: bold;">Qbeela</span> helps you stay connected with places live at or care about
+                <ul>
+                  <li>Follow places you live at or care about</li>
+                  <li>Get personalized information from government and social institutions</li>
+                  <li>Raise issues that affect you and your community</li>
+                  <li>Discuss and act for impact</li>
+                  <li>Communicate with authorities</li>
+                  <li>Bring change, together</li>
+                </ul>
+              </div>
+            </el-col>
+            <el-col :span="8">
+              <div class="login-or-signup">
+                <div v-if="loginOrSignup === 'login'">
+                  <login-form-vertical></login-form-vertical>
+                  <el-row class="login-or-signup__alter-links">
+                    <el-col :span="12">
+                      <el-button type="text" @click="toggleLoginSignup('signup')">New User? SignUp</el-button>
+                    </el-col>
+                    <!--<el-col :span="12">-->
+                      <!--<el-button type="text" @click="$router.push({name: 'forgot-password'})">Forgot Password?</el-button>-->
+                    <!--</el-col>-->
+                  </el-row>
+                </div>
+                <div v-if="loginOrSignup === 'signup'">
+                  <signup-form-vertical></signup-form-vertical>
+                  <el-row class="login-or-signup__alter-links">
+                    <el-col>
+                      <el-button type="text" @click="toggleLoginSignup('login')">Joined already? Login</el-button>
+                    </el-col>
+                  </el-row>
+                </div>
+              </div>
             </el-col>
           </el-row>
         </div>
@@ -24,6 +56,7 @@ import HeaderGuest from '@/components/layouts/headers/Guest'
 import BodyVerticalScroll from '@/components/layouts/body/VerticalScroll'
 
 import LoginFormVertical from '@/components/auth/LoginFormVertical'
+import SignupFormVertical from '@/components/auth/SignupFormVertical'
 
 export default {
   name: 'home',
@@ -31,7 +64,32 @@ export default {
     BodyVerticalScroll,
     HeaderGuest,
     MainLayout,
-    LoginFormVertical
+    LoginFormVertical,
+    SignupFormVertical
+  },
+  data () {
+    return {
+      loginOrSignup: 'login'
+    }
+  },
+  methods: {
+    toggleLoginSignup (loginOrSignup) {
+      this.loginOrSignup = loginOrSignup
+    }
   }
 }
 </script>
+<style>
+  .description {
+    font-size: 20px;
+  }
+  .login-or-signup__alter-links {
+    margin-top: 15px;
+  }
+  .login-or-signup {
+    padding: 20px;
+    border: 1px solid #e5e5e5;
+    background: #f5f5f5;
+    border-radius: 10px;
+  }
+</style>
